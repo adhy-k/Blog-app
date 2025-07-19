88d60eda60a7eaa2156ef421921a1f2dbcc7387c
adhy-k <adhykrishna123@gmail.com>
2025-07-19 13:42:45 +0530

    signup api created

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const cors=require("cors")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
-const app=express()
+const app = express()
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect("mongodb+srv://adhy:kannan123@cluster0.v6yc0yl.mongodb.net/blogAppDB?retryWrites=true&w=majority&appName=Cluster0")
+
+app.get("/signup", async (req, res) => {
+    let userData = req.body
+    let hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+
+    let checkEmail = userModel.find({ email: req.body.email }).then(
+        (item) => {
+            if (item.length > 0) {
+                res.json({ "Status": "Email already exist" })
+            }
+            else {
+                userData.save()
+                res.json({ "Status": "Sucess" })
+            }
+        }
+    ).catch()
 })
 
-app.listen(4000,(error)=>{console.log("Server Running")+error})
+
+app.listen(4000, (error) => { console.log("Server Running") + error })
